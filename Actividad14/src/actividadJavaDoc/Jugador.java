b5206fab78f68fae74920bfda3043955a28fe3b8
LuisG <lggsanchez21@gmail.com>
2024-04-15 14:07:00 +0200

    version final

--- Actividad14/src/actividadJavaDoc/Jugador.java
+++ Actividad14/src/actividadJavaDoc/Jugador.java
@@ -37,22 +37,12 @@ public class Jugador {
 	}
 	
 
-	//El método crea una variable llamda dorsal de tipo numérica,
-	//los numeros a poner serán entre el 1 y el 30
 
 	/**
 	 * Metodo que hace la funcion de poner Dorsal al jugador
 	 * Verifica si el dorsal es mayor o igual a 1 y menor o igual a 30, se le designa el dorsal pasado por 
 	 * parametro, caso contrario el dorsal sera -1
 	 * @param dorsal Representa el dorsal que queremos designar
-	 */
-
-	/**
-	 * Metodo que hace que el jugador se ponga un dorsal pasado por parametro. Si el número 
-	 * del dorsal está entre 1 y 30, se pone el dorsal, de lo contrario el valor del dorsal será 
-	 * -1
-	 * 
-	 * @param dorsal
 	 */
 
 	public void ponerDorsal(int dorsal) {
@@ -63,27 +53,15 @@ public class Jugador {
 		}
 	}
 	
-
-
-	//El crea una variable llamada estaExpulsado la cual te dice
-	//que si tiene 2 amarrillas está expulsado o si tiene una 
-	//cartulina roja. Si no, el jugador no estaría expulsado
-
 /**
  * Metodo que devuelve si esta expulsado
  * Primero designa que no esta expulsado, luego condiciona que
- * si el "numeroTarjetasAmarillas" es igual a 2, devuelve que sera expulsado.
- * Luego si tiene numeroTarjetasRojas es igual a 1, devuelve que sera expulsado
- * @return devuelve si el juegador esta expulsado o no, dependiendo a las tarjetas que tiene
+ * si el "numeroTarjetasAmarillas" es igual a 2 o si el "numeroTarjetasRojas" 
+ * es igual a 1, el jugador será expulsado
+ * @return true si el juegador esta expulsado, false si no esta expulsado
  */
 
-	/**
-	 * Metodo que devuelve si un jugador ha sido expulsado. Si el jugador tiene 2 tarjetas amarillas
-	 * o una tarjeta, está expulsado, de lo contrario, no será expulsado.
-	 * 
-	 * @return "True" si ha sido expulsado, "False" si no ha sido expulsado
-	 */
-	
+
 	public boolean estaExpulsado() {
 		boolean expulsado = false;
 		if (numeroTarjetasAmarillas == 2) {
--- Actividad14/src/actividadJavaDoc/Soldado.java
+++ Actividad14/src/actividadJavaDoc/Soldado.java
@@ -1,41 +1,35 @@
 package actividadJavaDoc;
 
 /**
- *Clase que representa dos atributos estaMuerto y numeroBalas
- *@author Felix
- *@version 1.0
+ * Clase que representa dos atributos estaMuerto y numeroBalas
+ * 
+ * @author Felix
+ * @version 1.0
  */
 public class Soldado {
-	
 
 	/**
 	 * Atributo booleano que determina si el soldado esta muerto o no.
 	 */
 	private boolean estaMuerto;
-	
+
 	/**
 	 * Atributo de valor entero que determina el numero de balas
 	 */
-
-	
 
 	private int numeroBalas;
 
 	// Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las
 	// pruebas y la documentación. Aquí no se han creado porque no aportan nada
 
-
-
 	public boolean isEstaMuerto() {
 		return estaMuerto;
 	}
 
-	
 	public void setEstaMuerto(boolean estaMuerto) {
 		this.estaMuerto = estaMuerto;
 	}
 
-	
 	public int getNumeroBalas() {
 		return numeroBalas;
 	}
@@ -43,23 +37,14 @@ public class Soldado {
 	public void setNumeroBalas(int numeroBalas) {
 		this.numeroBalas = numeroBalas;
 	}
-	
-	//Crea una variable llamada puedeDisparar, la cual
-		//te dice que si el numero de balas es mayor que 
-		// 0 puede disparar, en cambio si tiene menos no
-	
 
-
-	//Crea una variable llamada puedeDisparar, la cual
-	//te dice que si el numero de balas es mayor que 
-	// 0 puede disparar, en cambio si tiene menos no
 
 	/**
-	 * Método que hace disparar al soldado contra otro soldado introducido por parametro. Al 
-	 * disparar pierde una bala del atributo "numeroBalas" y el atributo "estaMuerto" del soldado introducido 
-	 * parametro es "True".
+	 * Método que hace disparar al soldado contra otro soldado introducido por
+	 * parametro. Al disparar pierde una bala del atributo "numeroBalas" y el
+	 * atributo "estaMuerto" del soldado introducido parametro es "True".
 	 * 
-	 * @param sol Soldado que es disparado 
+	 * @param sol Soldado que es disparado
 	 */
 	public void disparar(Soldado sol) {
 
@@ -70,8 +55,8 @@ public class Soldado {
 	}
 
 	/**
-	 * Método que devuelve si un soldado puedo disparar. El soldado puede disparar si su 
-	 * número de balas es mayor a 0, de caso contrario no podrá disparar.
+	 * Método que devuelve si un soldado puedo disparar. El soldado puede disparar
+	 * si su número de balas es mayor a 0, de caso contrario no podrá disparar.
 	 * 
 	 * @return "True" si puede disparar, "False" si no puede disparar
 	 */
@@ -82,13 +67,9 @@ public class Soldado {
 		}
 		return false;
 	}
-	//Crea la variable disparar al soldado sol, lo que
-		//hace es ir reduciendo el número de balas y saber 
-		// si sol esta muerto
-	
-	//El crea una variable llamada estaExpulsado la cual te dice
-		//que si tiene 2 amarrillas está expulsado o si tiene una 
-		//cartulina roja. Si no, el jugador no estaría expulsado
+	// Crea la variable disparar al soldado sol, lo que
+	// hace es ir reduciendo el número de balas y saber
+	// si sol esta muerto
 
 
-	}
+}
