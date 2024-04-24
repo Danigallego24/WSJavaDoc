c2f6969b01378dd77de93caf7007e4e2f5fbe2f9
LuisG <lggsanchez21@gmail.com>
2024-04-24 09:23:47 +0200

    add Pruebas Unitarias Luis

--- Actividad14/src/actividadJavaDoc/MainPruebaJugador.java
+++ Actividad14/src/actividadJavaDoc/MainPruebaJugador.java
@@ -3,8 +3,124 @@ package actividadJavaDoc;
 public class MainPruebaJugador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("------------------Luis--------------------");
 
+		//Vamos a probar la clase jugador, definiendo su 
+		//bateria de pruebas y ejecutando sus métodos
+		
+		//Método ponerDorsal()
+		//Vamos a hacer tres pruebas para este método
+		
+		//Prueba 1
+		//dorsal = 0
+		//Resultado esperado = -1
+		
+		//Ejecutamos la prueba 1
+		Jugador j1 = new Jugador();
+		int resultadoEsperado = -1;
+		int dorsal = j1.getDorsal();
+		j1.ponerDorsal(dorsal);
+		int resultadoObtenido = j1.getDorsal();
+		
+		if(resultadoEsperado == resultadoObtenido) {
+			System.out.println("ponerDorsal() Prueba 1 -> OK");
+		}else {
+			System.out.println("ponerDorsal() Prueba 1 -> KO");
+		}
+		
+		//Prueba 2
+		//dorsal = 10
+		//Resultado esperado = 10
+		
+		//Ejecutamos la prueba 2
+		Jugador j2 = new Jugador();
+		resultadoEsperado = 10;
+		dorsal = 10;
+		j2.ponerDorsal(dorsal);
+		resultadoObtenido = j2.getDorsal();
+		
+		if(resultadoEsperado == resultadoObtenido) {
+			System.out.println("ponerDorsal() Prueba 2 -> OK");
+		}else {
+			System.out.println("ponerDorsal() Prueba 2 -> KO");
+		}
+				
+
+		//Prueba 3
+		//dorsal = 31
+		//Resultado esperado = -1
+		
+		//Ejecutamos la prueba 3
+		Jugador j3 = new Jugador();
+		resultadoEsperado = -1;
+		dorsal = 31;
+		j3.ponerDorsal(dorsal);
+		resultadoObtenido = j3.getDorsal();
+		
+		if(resultadoEsperado == resultadoObtenido) {
+			System.out.println("ponerDorsal() Prueba 3 -> OK");
+		}else {
+			System.out.println("ponerDorsal() Prueba 3 -> KO");
+		}
+		
+		//Método estaExpulsado()
+		//Vamos a hacer tres pruebas para este método
+		
+		//Prueba 1 
+		//numeroTarjetasAmarillas = 0
+		//numeroTarjetasRojas = 1
+		//Resultado esperado = true
+		j1 = new Jugador();
+		j1.setNumeroTarjetasAmarillas(0);
+		j1.setNumeroTarjetasRojas(1);
+		boolean resultadoEsperado2 = true;
+		boolean resultadoObtenido2 = j1.estaExpulsado();
+		
+		if(resultadoEsperado2 == resultadoObtenido2 ) {
+			System.out.println("estaExpulsado() Prueba 1 -> OK");
+		}else {
+			System.out.println("estaExpulsado() Prueba 1 -> OK");
+		}
+		
+		//Prueba 2 
+		//numeroTarjetasAmarillas = 2
+		//numeroTarjetasRojas = 0
+		//Resultado Esperado = true
+		
+		j2 = new Jugador();
+		j2.setNumeroTarjetasAmarillas(2);
+		j2.setNumeroTarjetasRojas(0);
+		resultadoEsperado2 = true;
+		resultadoObtenido2 = j2.estaExpulsado();
+		
+		if(resultadoEsperado2 == resultadoObtenido2 ) {
+			System.out.println("estaExpulsado() Prueba 2 -> OK");
+		}else {
+			System.out.println("estaExpulsado() Prueba 2 -> OK");
+		}
+		
+		//Prueba 3 
+		//numeroTarjetasAmarillas = 40
+		//numeroTarjetasRojas = 20
+		//Resultado esperado = false
+		
+		j3 = new Jugador();
+		j3.setNumeroTarjetasAmarillas(40);
+		j3.setNumeroTarjetasRojas(20);
+		resultadoEsperado2 = false;
+		resultadoObtenido2 = j3.estaExpulsado();
+		
+		if(resultadoEsperado2 == resultadoObtenido2 ) {
+			System.out.println("estaExpulsado() Prueba 1 -> OK");
+		}else {
+			System.out.println("estaExpulsado() Prueba 1 -> OK");
+		}
+		
+		System.out.println("------------------Fin Luis--------------------");
+
+		
+		
 	}
 
 }
--- Actividad14/src/actividadJavaDoc/MainPruebaSoldado.java
+++ Actividad14/src/actividadJavaDoc/MainPruebaSoldado.java
@@ -4,5 +4,92 @@ public class MainPruebaSoldado {
 
 	public static void main(String[] args) {
 		
+		System.out.println("------------------Luis--------------------");
+		//Vamos a probar la clase soldado, definiendo su 
+		//bateria de pruebas y ejecutando sus métodos
+		
+		//Método disparar()
+		//Vamos a hacer dos pruebas para este método
+		
+		//Prueba 1
+		//this.numeroBalas = 1
+		//Resultado esperado = 0
+		//sol.estaMuerto() = false
+		//Resultado esperado = true
+		
+		//Ejecutamos la prueba 1
+		Soldado soldadoQueDispara1 = new Soldado();
+		Soldado soldadoDisparado1 = new Soldado();
+		soldadoQueDispara1.setNumeroBalas(1);
+		int resultadoEsperadoBalas = 0;
+		soldadoDisparado1.setEstaMuerto(false);
+		boolean resultadoEsperadoMuerto = true;
+		soldadoQueDispara1.disparar(soldadoDisparado1);
+		
+		if(resultadoEsperadoMuerto == soldadoDisparado1.isEstaMuerto() && resultadoEsperadoBalas == soldadoQueDispara1.getNumeroBalas()) {
+			System.out.println("disparar() Prueba 1 -> OK");
+		}else {
+			System.out.println("disparar() Prueba 1  -> KO");
+		}
+		
+		
+		//Prueba 2
+		//this.numeroBalas = -7
+		//Resultado esperado = -8
+		//sol.estaMuerto() = true
+		//Resultado esperado = true
+	
+		//Ejecutamos la prueba 2
+		Soldado soldadoQueDispara2 = new Soldado();
+		Soldado soldadoDisparado2 = new Soldado();
+		soldadoQueDispara2.setNumeroBalas(-7);
+		resultadoEsperadoBalas = -8;
+		soldadoDisparado2.setEstaMuerto(true);
+		resultadoEsperadoMuerto = true;
+		soldadoQueDispara2.disparar(soldadoDisparado2);
+		
+		if(resultadoEsperadoMuerto == soldadoDisparado2.isEstaMuerto() && resultadoEsperadoBalas == soldadoQueDispara2.getNumeroBalas()) {
+			System.out.println("disparar() Prueba 2 -> OK");
+		}else {
+			System.out.println("disparar() Prueba 2  -> KO");
+		}
+		
+		//Metodo puedeDisparar()
+		//Vamos a hacer 2 pruebas 
+		
+		//Prueba 1 
+		//numeroBalas = 5
+		//Resultado esperado = true
+		
+		//Ejecutamos la prueba 1
+		Soldado s1 = new Soldado();
+		s1.setNumeroBalas(5);
+		boolean resultadoEsperado = true;
+		boolean resultadoObtenido = s1.puedeDisparar();
+		
+		if(resultadoObtenido == resultadoEsperado) {
+			System.out.println("puedeDisparar() Prueba 1 -> OK");
+		}else {
+			System.out.println("puedeDisparar() Prueba 1  -> KO");
+		}
+		
+		//Prueba 2
+		//numeroBalas = 0
+		//Resultado esperado = false
+		
+		//Ejecutamos la prueba 2
+		Soldado s2 = new Soldado();
+		s2.setNumeroBalas(0);
+		resultadoEsperado = false;
+		resultadoObtenido = s2.puedeDisparar();
+		
+		if(resultadoObtenido == resultadoEsperado) {
+			System.out.println("puedeDisparar() Prueba 1 -> OK");
+		}else {
+			System.out.println("puedeDisparar() Prueba 1  -> KO");
+		}
+		
+		System.out.println("------------------Fin Luis--------------------");
+
 	}
 }
